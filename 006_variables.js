a909cebab5adeb8fb2703d1350f1133f69c7d97a
CherishClark <cherish.g.clark@gmail.com>
2018-01-29 14:39:17 -0700

    learning interpolation

--- 006_variables.js
+++ 006_variables.js
@@ -1,6 +1,8 @@
 // Only three variables in Typescript
 // use camel case, declar type of variable, name of variable, :, datatype
 // const means constant, tells program you do not want variable changed
+// not allowed to REDEFINE a let statement, just reset the value. 
+// var, you can change the value, but you need to keep same data type
 // var fullName : string = "Cherish Clark";
 // let paidAccount : boolean = true;
 // const versionNumber : number = 1.3;
@@ -9,9 +11,15 @@
 // console.log(fullName);
 // console.log(paidAccount);
 // console.log(versionNumber);
-function printName(f, l) {
-    var greeting = "Hi there, ";
-    console.log(greeting + f + " " + l);
-}
-printName("Cherish", "Clark");
+// function printName(f, l) {
+//   var greeting : string = "Hi there, ";
+//   console.log(greeting + f + " " + l);
+//    var greeting : string = "Howdy there, ";
+//   console.log(greeting + f + " " + l);
+// }
+// printName("Cherish", "Clark");
+// interpolation
+var msg = "Danielle";
+console.log("A message to my sister" + msg + "filled with text");
+console.log("A long message to " + msg + " filled with text");
 //# sourceMappingURL=006_variables.js.map
--- 006_variables.ts
+++ 006_variables.ts
@@ -2,10 +2,13 @@
 
 // use camel case, declar type of variable, name of variable, :, datatype
 // const means constant, tells program you do not want variable changed
+// not allowed to REDEFINE a let statement, just reset the value. 
+// var, you can change the value, but you need to keep same data type
 
 // var fullName : string = "Cherish Clark";
 // let paidAccount : boolean = true;
 // const versionNumber : number = 1.3;
+
 
 // fullName = "Danielle Clark";
 // paidAccount = false;
@@ -14,9 +17,18 @@
 // console.log(paidAccount);
 // console.log(versionNumber);
 
-function printName(f, l) {
-  var greeting : string = "Hi there, ";
-  console.log(greeting + f + " " + l);
-}
+// function printName(f, l) {
+//   var greeting : string = "Hi there, ";
+//   console.log(greeting + f + " " + l);
 
-printName("Cherish", "Clark");
+//    var greeting : string = "Howdy there, ";
+//   console.log(greeting + f + " " + l);
+// }
+
+// printName("Cherish", "Clark");
+
+// interpolation
+
+var msg : string = "Danielle";
+console.log("A message to my sister" + msg + "filled with text");
+console.log(`A long message to ${msg} filled with text`);
