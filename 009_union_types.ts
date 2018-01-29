da3af8d549390c8f07cb5bbd786d35060df78831
CherishClark <cherish.g.clark@gmail.com>
2018-01-29 15:39:07 -0700

    Learning union types in TypeScript

---
+++ 009_union_types.ts
@@ -0,0 +1,14 @@
+type PlayerArray = Array<string|number>;
+let players : PlayerArray = ["Altuve", "Corerra", "bregman"];
+let player_numbers : PlayerArray = [25, 3, 2];
+
+console.log(players);
+console.log(player_numbers);
+
+var names : string[]|string;
+names = ["Cherish", "danny"];
+
+console.log(names);
+names = "cherish";
+
+console.log(names);
