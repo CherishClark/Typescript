60faabcf6391ebacd88b6264743294fc66beb09c
CherishClark <cherish.g.clark@gmail.com>
2018-01-29 14:25:23 -0700

    learning how to write functions in type script with var variable

--- 006_variables.js
+++ 006_variables.js
@@ -1,10 +1,17 @@
 // Only three variables in Typescript
 // use camel case, declar type of variable, name of variable, :, datatype
 // const means constant, tells program you do not want variable changed
-var fullName = "Cherish Clark";
-var paidAccount = true;
-var versionNumber = 1.3;
-console.log(fullName);
-console.log(paidAccount);
-console.log(versionNumber);
+// var fullName : string = "Cherish Clark";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Danielle Clark";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Cherish", "Clark");
 //# sourceMappingURL=006_variables.js.map
--- 006_variables.ts
+++ 006_variables.ts
@@ -3,10 +3,20 @@
 // use camel case, declar type of variable, name of variable, :, datatype
 // const means constant, tells program you do not want variable changed
 
-var fullName : string = "Cherish Clark";
-let paidAccount : boolean = true;
-const versionNumber : number = 1.3;
+// var fullName : string = "Cherish Clark";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
 
-console.log(fullName);
-console.log(paidAccount);
-console.log(versionNumber);
+// fullName = "Danielle Clark";
+// paidAccount = false;
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+function printName(f, l) {
+  var greeting : string = "Hi there, ";
+  console.log(greeting + f + " " + l);
+}
+
+printName("Cherish", "Clark");
