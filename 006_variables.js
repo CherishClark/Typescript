d71ebc4c940689cf52ac2e25ba9ea489fae54be3
CherishClark <cherish.g.clark@gmail.com>
2018-01-29 14:12:30 -0700

    learning different variable types in TypeScript

---
+++ 006_variables.js
@@ -0,0 +1,10 @@
+// Only three variables in Typescript
+// use camel case, declar type of variable, name of variable, :, datatype
+// const means constant, tells program you do not want variable changed
+var fullName = "Cherish Clark";
+var paidAccount = true;
+var versionNumber = 1.3;
+console.log(fullName);
+console.log(paidAccount);
+console.log(versionNumber);
+//# sourceMappingURL=006_variables.js.map
---
+++ 006_variables.ts
@@ -0,0 +1,12 @@
+// Only three variables in Typescript
+
+// use camel case, declar type of variable, name of variable, :, datatype
+// const means constant, tells program you do not want variable changed
+
+var fullName : string = "Cherish Clark";
+let paidAccount : boolean = true;
+const versionNumber : number = 1.3;
+
+console.log(fullName);
+console.log(paidAccount);
+console.log(versionNumber);
