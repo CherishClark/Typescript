53d76fe9c16b4bc6c4a49c9775f21ff129e914ef
CherishClark <cherish.g.clark@gmail.com>
2018-01-30 16:31:26 -0700

    learning closures:

--- 018_immediately_invoked_functions.js
+++ 018_immediately_invoked_functions.js
@@ -1,9 +1,19 @@
-var names = ['Jordan', 'Cherish', 'Paul'];
-var counter = 0;
-(function () {
-    for (var name_1 in names) {
-        counter++;
-    }
-})();
-console.log(counter);
+// var names : string[] = ['Jordan', 'Cherish', 'Paul'];
+// var counter : number = 0;
+// (function() {
+//   for (let name in names) {
+//     counter++;
+//   }
+// })();
+// console.log(counter);
+// a function expression
+var fullName;
+fullName = function (first, last) {
+    return first + " " + last;
+};
+console.log(fullName('chersh', 'clark'));
+//  immediately invoked version
+(function (first, last) {
+    console.log(first + " " + last);
+})('Cherish', 'Clark');
 //# sourceMappingURL=018_immediately_invoked_functions.js.map
--- 018_immediately_invoked_functions.ts
+++ 018_immediately_invoked_functions.ts
@@ -1,10 +1,25 @@
-var names : string[] = ['Jordan', 'Cherish', 'Paul'];
-var counter : number = 0;
+// var names : string[] = ['Jordan', 'Cherish', 'Paul'];
+// var counter : number = 0;
 
-(function() {
-  for (let name in names) {
-    counter++;
-  }
-})();
-console.log(counter);
+// (function() {
+//   for (let name in names) {
+//     counter++;
+//   }
+// })();
+// console.log(counter);
 
+// a function expression
+
+var fullName : (first : string, last : string) => string;
+
+fullName = function(first : string, last : string) {
+  return first + " " + last;
+}
+
+console.log(fullName('chersh', 'clark'));
+
+//  immediately invoked version
+
+(function(first : string, last : string) {
+  console.log(first + " " + last);
+})('Cherish', 'Clark');
